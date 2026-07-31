6d22291ae52b8ee9fbf08f7c953982c931e2593f
lpriya-m <53342063+lpriya-m@users.noreply.github.com>
2026-07-31 14:01:37 +0530

    Revise personal description and add funDescription
    Updated the description and added funDescription.

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -23,11 +23,12 @@ export const aboutMe: AboutMe = {
   institution: "Dolby Laboratories",
   // Note that links work in the description
   description:
-    "I’ve always been drawn to the creative side of technology, especially where graphics, perception, and interaction meet. After completing my PhD in computer graphics at IIT Madras, I moved into industry research to work on problems that combine mathematical beauty with user-centered design. I’ve contributed to 3D graphics research, patents, XR prototypes, and engineering solutions that support real-time rendering and visual computing workflows. What motivates me is building tools that make visual creation easier, faster, and more expressive. I value thoughtful execution, collaboration, and learning environments where ideas grow collectively.",
+    "I’ve always been drawn to the creative side of technology, especially where graphics, perception, and interaction meet. After completing my PhD in 3D Computer Graphics at IIT Madras, I moved into industry research to work on problems that combine mathematical beauty with user-centered design.<br> I’ve contributed to 3D graphics research, patents, XR prototypes, and engineering solutions that support real-time rendering and visual computing workflows. What motivates me is building tools that make visual creation easier, faster, and more expressive. I value thoughtful execution, collaboration, and learning environments where ideas grow collectively.",
   imageUrl:"/lakshmi.jpg",
   email: "lakshmipriya369@gmail.com",
   googleScholarUrl: "https://scholar.google.com/citations?user=WB628xAAAAAJ",
   githubUsername: "lpriya-m",
   linkedinUsername: "lakshmipriyam",
   cvUrl: "https://lpriya-m.github.io/cv/cv.pdf",
+  funDescription: "Outside of work, I enjoy reading, exploring new destinations and Indian classical dance" 
 };
